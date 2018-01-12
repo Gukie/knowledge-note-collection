2893a1e4d97bcf255d3dbe1d26a65ce465995709
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2018-01-12 18:12:43 +0800

    add echo

--- src/main/java/com/lokia/demo/netty/handler/DiscardServerHandler.java
+++ src/main/java/com/lokia/demo/netty/handler/DiscardServerHandler.java
@@ -1,5 +1,7 @@
 package com.lokia.demo.netty.handler;
 
+import java.io.UnsupportedEncodingException;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
@@ -13,25 +15,46 @@ public class DiscardServerHandler extends ChannelHandlerAdapter {
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 //		super.channelRead(ctx, msg);
+		readMsg(msg);
+		echoMsg(ctx,msg);
+	}
+
+	private void echoMsg(ChannelHandlerContext ctx,Object receivedMsg) {
+		String echoMsgPrefix = "I have received ";
+		String receivedMsgStr = buildMsgStr((ByteBuf)receivedMsg);
+		System.out.println("echo to server:"+(echoMsgPrefix+receivedMsgStr));
+		logger.debug("echo to server:{}",(echoMsgPrefix+receivedMsgStr));
+		ctx.writeAndFlush(echoMsgPrefix+receivedMsgStr);
+		
+	}
+
+	private void readMsg(Object msg) {
 		System.out.println("received msg:"+msg);
 		logger.debug("received msg:{}",msg);
 		if(msg instanceof ByteBuf){
-			String msgStr =getMsgStr((ByteBuf)msg);
+			String msgStr =buildMsgStr((ByteBuf)msg);
 			System.out.println("received msg:"+msgStr);
 			logger.debug("received msg:{}",msgStr);
 		}
 	}
 
-	private String getMsgStr(ByteBuf buf) {
-		int size = buf.readableBytes();
+	private String buildMsgStr(ByteBuf original) {
+		ByteBuf currentData = original.copy(); // copy, in order to reuse.
+		int size = currentData.readableBytes();
 		byte[] bytes = new byte[size];
 		int index = 0;
-		while(buf.readableBytes()>0){
-			byte bt = buf.readByte();
+		while(currentData.readableBytes()>0){
+			byte bt = currentData.readByte();
 			bytes[index] = bt;
 			index++;
 		}
-		return new String(bytes);
+		try {
+			return new String(bytes,"utf-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			logger.error("Exception happens when building", e);
+		}
+		return "";
 	}
 	
 	@Override
