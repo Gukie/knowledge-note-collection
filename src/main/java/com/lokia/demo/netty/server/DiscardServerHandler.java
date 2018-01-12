a19f87d2be7d936213a73457754ec24ddf40b747
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2018-01-12 10:05:36 +0800

    change the netty version and some code

--- src/main/java/com/lokia/demo/netty/server/DiscardServerHandler.java
+++ src/main/java/com/lokia/demo/netty/server/DiscardServerHandler.java
@@ -1,25 +1,20 @@
 package com.lokia.demo.netty.server;
 
-import io.netty.buffer.ChannelBuf;
+import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 
-public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+public class DiscardServerHandler extends ChannelHandlerAdapter {
 
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		// TODO Auto-generated method stub
+		super.channelRead(ctx, msg);
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		// TODO Auto-generated method stub
+		super.exceptionCaught(ctx, cause);
+	}
 
-    @Override
-    public void inboundBufferUpdated(ChannelHandlerContext channelHandlerContext) throws Exception {
-
-
-    }
-
-    @Override
-    public ChannelBuf newInboundBuffer(ChannelHandlerContext channelHandlerContext) throws Exception {
-        return null;
-    }
-
-    @Override
-    public void freeInboundBuffer(ChannelHandlerContext channelHandlerContext, ChannelBuf channelBuf) throws Exception {
-
-    }
 }
